2bf84830219fa1d78cda0d2c6c88aaf1b41d72a0
sounkalo20 <sidibesounk2003@gmail.com>
2023-01-18 08:44:53 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [taches, setTaches] = useState([
+    { id: 1, nom: "faire la lessive" },
+    { id: 2, nom: "faire le menage" },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {taches.map((tache) => {
+        return (
+          <li>
+            {tache.nom} <button>X</button>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
